8dfffcc6bfc14b485d4ec7b5d227e8b61de8b640
Maicheal Shenouda <rory1980rory1980@gmail.com>
2025-10-02 21:37:30 -0400

    Added Javadoc.

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Album"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Book"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"MediaItem"},{"p":"<Unnamed>","l":"MediaParser"},{"p":"<Unnamed>","l":"Movie"}];updateSearchResults();
